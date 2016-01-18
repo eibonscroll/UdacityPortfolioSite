be8f86dd77179f0fe461f95cd08048fc5e1f0b6e
eibonscroll <mikepurvis7@gmail.com>
2016-01-18 13:42:31 -0600

    Submission 1
    Submission 1 for the Udacity Front End NanoDegree Portfolio Project

---
+++ js/regrid.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Mike on 1/18/2016.
+ */
+
+var changeMainImage = function () {
+    if( (window.matchMedia("(min-width:600px)").matches)){
+        $('#mainImage').attr("src", "img/board.jpg");
+        $('#mainImage').attr("alt", "a motherboard");
+    }
+    else
+    {
+        $('#mainImage').attr("src", "img/sumo.jpg");
+        $('#mainImage').attr("alt", "a sumo robot");
+    }
+}
+
+$(window).resize(function () {
+    //resize just happened, pixels changed
+    changeMainImage ();
+});
+
+$(document).ready(function () {
+    changeMainImage ();
+
+});
+
