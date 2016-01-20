0f082846e672368dd16ce257e3b07651247294c8
Mike <mikepurvis7@gmail.com>
2016-01-19 21:07:58 -0600

    feat: Moving Silon
    Adds animating Silon head under header
    
    removes style tag from html
    
    adjusts srcset for icon
    
    removes extra </a> tag
    
    resolves: failed submission due to style in html

--- js/regrid.js
+++ js/regrid.js
@@ -1,26 +1,80 @@
 /**
  * Created by Mike on 1/18/2016.
  */
+/*jslint browser: true*/ /*global  $*/
 
-var changeMainImage = function () {
-    if( (window.matchMedia("(min-width:600px)").matches)){
-        $('#mainImage').attr("src", "img/board.jpg");
-        $('#mainImage').attr("alt", "a motherboard");
+var silon_speed = 2;
+var update_rate = 5;
+var right_bound = 0;
+var left_bound = 0;
+var silon_track_left = 0;
+var silon_width = 0;
+var silon_quarter_width = 0;
+var slide_right = true;
+var timer = null;
+
+var updateSilon = function () {
+    var head = $(".silon");
+    var x = parseInt(head.offset().left);
+
+    //test if going right
+    if(slide_right) {
+        //test if going to hit right edge
+        if (x < right_bound) {
+            x = x + silon_speed;
+        }
+        else {
+            //set not going right if it was going to hit
+            slide_right = false;
+        }
     }
-    else
+    //test if not going right
+    if(!slide_right)
     {
-        $('#mainImage').attr("src", "img/sumo.jpg");
-        $('#mainImage').attr("alt", "a sumo robot");
+        //test if greater than the left edge
+        if(x > left_bound)
+        {
+            x = x - silon_speed;
+        }
+        else
+        {
+            //change direction to right
+            slide_right = true;
+        }
     }
-};
+
+    $(".silon").offset({left: x});
+}
+
+var initSilon = function () {
+    slide_right = true;
+    silon_width = $(".silon").width();
+    silon_quarter_width = silon_width / 4;
+    //get left of row silon is in
+    silon_track_left = parseInt($(".silon_track").offset().left);
+    //get right of row silon is in
+    right_bound = $(".silon_track").width() + silon_track_left;
+    //set left bound
+    left_bound = silon_track_left - silon_quarter_width;
+
+    //set starting position of silon
+    $(".silon").offset({left: left_bound});
+
+    //if timer running, stop it
+    if(timer !== null)
+    {
+        timer.stop();
+        timer = null;
+    }
+    timer = setInterval(updateSilon,update_rate);
+}
 
 $(window).resize(function () {
     //resize just happened, pixels changed
-    changeMainImage ();
+    initSilon();
 });
 
 $(document).ready(function () {
-    changeMainImage ();
-
+    initSilon();
 });
 
